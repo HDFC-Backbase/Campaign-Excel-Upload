09d89147b9018de8cbe038f4604e160580a830a5
deepti.surve@transform-hub.com <deepti.surve@transform-hub.com>
2021-01-11 19:30:49 +0530

    Change corporate_offer_staging.

--- campaignupload-service/src/main/java/com/backbase/campaignupload/entity/CorporateStagingEntity.java
+++ campaignupload-service/src/main/java/com/backbase/campaignupload/entity/CorporateStagingEntity.java
@@ -10,7 +10,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "corp_staging")
+@Table(name = "corporate_offer_staging")
 public class CorporateStagingEntity {
 
 
