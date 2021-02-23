46e4d7808ef935e9f5473a96b1a8ea7286f32fdd
Mayur <mayurera9@gmail.com>
2021-02-23 16:23:20 +0530

    Added get query changes

--- campaignupload-service/src/main/java/com/backbase/campaignupload/repo/CorporateOfferRepo.java
+++ campaignupload-service/src/main/java/com/backbase/campaignupload/repo/CorporateOfferRepo.java
@@ -9,6 +9,6 @@ import com.backbase.campaignupload.entity.CorporateStagingEntity;
 
 public interface CorporateOfferRepo extends JpaRepository<CorporateStagingEntity,Integer>,CorporateStagCustomRepo{
 	
-	@Query(value="select * from corporate_offer_staging crp where crp.id not in(select costg.id from corporate_offer_staging costg where costg.crp_status='Pending' and costg.file_id is not null) order by crp.updated_on desc",nativeQuery=true)
+	@Query(value="select * from corporate_offer_staging crp where crp.id not in(select costg.id from corporate_offer_staging costg where costg.crp_status='Pending' and costg.file_id is not null and costg.id not in(select crinp.id from corporate_offer_staging crinp inner join corporate_offer_final crpf on crinp.id=crpf.crp_s_id) ) order by crp.updated_on desc",nativeQuery=true)
 	public List<CorporateStagingEntity> findAll();
 }
--- campaignupload-service/src/main/java/com/backbase/campaignupload/repo/PartnerOfferRepo.java
+++ campaignupload-service/src/main/java/com/backbase/campaignupload/repo/PartnerOfferRepo.java
@@ -9,6 +9,6 @@ import com.backbase.campaignupload.entity.PartnerOffersStagingEntity;
 
 public interface PartnerOfferRepo extends JpaRepository<PartnerOffersStagingEntity,Integer>,PTStagCustomRepo{
 	
-	@Query(value="select * from partner_offer_staging p where p.id not in(select pstg.id from partner_offer_staging pstg where pstg.pt_status='Pending' and pstg.file_id is not null) order by p.updated_on desc",nativeQuery=true)
+	@Query(value="select * from partner_offer_staging p where p.id not in(select pstg.id from partner_offer_staging pstg where pstg.pt_status='Pending' and pstg.file_id is not null and pstg.id not in(select prtin.id from partner_offer_staging prtin inner join partner_offer_final prtf on prtin.id=prtf.p_s_id) ) order by p.updated_on desc",nativeQuery=true)
 	public List<PartnerOffersStagingEntity> findAll();
 }
